069d5aedf3ecc2b81b555027c42808657f2912d0
Abishek.S <90471014+Abishek-Suresh@users.noreply.github.com>
2023-11-16 23:54:07 +0530

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -73,6 +73,24 @@ FROM
     EmployeeUNI u
     ON
     e.id = u.id
+;
+
+
+-- 1068. Product Sales Analysis I
+-- Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+SELECT
+    p.product_name,
+    s.year,
+    s.price
+FROM
+    Sales s
+    INNER JOIN
+    Product p ON
+    s.product_id = p.product_id
+ORDER BY
+    p.product_name
+
+-- 
 
 
 
